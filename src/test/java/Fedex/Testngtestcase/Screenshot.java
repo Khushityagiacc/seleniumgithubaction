33514362dad59eb4004feee18e3090b477e135ac
Khushityagiacc <khushi.a.tyagi@accenture.com>
2026-02-10 15:23:49 +0530

    Create Screenshot.java

---
+++ src/test/java/Fedex/Testngtestcase/Screenshot.java
@@ -0,0 +1,32 @@
+package Fedex.Testngtestcase;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.openqa.selenium.*;
+
+public class Screenshot {
+
+    public static String takeScreenshot(WebDriver driver, String name) {
+
+        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+        String screenshotDir = "test-output/screenshots";
+        String fileName = name + "_" + timestamp + ".png";
+        String fullPath = screenshotDir + "/" + fileName;
+
+        try {
+            Files.createDirectories(new File(screenshotDir).toPath());
+            Files.copy(src.toPath(), new File(fullPath).toPath());
+        } catch (IOException e) {
+            System.out.println("Screenshot failed: " + e.getMessage());
+        }
+
+        // Path RELATIVE to ExtentReport.html
+        return "screenshots/" + fileName;
+    }
+}
